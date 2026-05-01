e83dd0c71ee907ff5d624d2e14ef5220fb3ace2b
colescottjury-tech <colescottjury@gmail.com>
2026-05-01 12:58:42 +0000

    cat

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include<cmath>
 
 class Point {
 
@@ -21,6 +22,10 @@ public:
         return y;
     }
 
+    double distanceTo(Point otherPoint){
+        return pow(pow(otherPoint.getX() - x, 2) + pow(otherPoint.getY() - y, 2), 0.5);
+    }
+
     std::string toString() {
         return "(" + std::to_string(x) + ", "
                    + std::to_string(y) + ")";
@@ -36,8 +41,24 @@ int main() {
     Point p1(0.0, 0.0);
     Point p2(3.0, 4.0);
 
+    std::cout << "Point?" << std::endl;
+    
+    std::cout << "X: ";
+    double xVal;
+    double yVal;
+    std::cin >> xVal;
+    std::cout << "Y: ";
+    std::cin >> yVal;
+
+    Point userPoint(xVal, yVal);
+
     std::cout << "p1 = " << p1.toString() << std::endl;
     std::cout << "p2 = " << p2.toString() << std::endl;
+    std::cout << "User = " << userPoint.toString() << std::endl;
+
+
+    std::cout << "distance = " << p1.distanceTo(p2) << std::endl;
+
 
     std::cout << "p2.x = " << p2.getX() << std::endl;
     std::cout << "p2.y = " << p2.getY() << std::endl;
